3fb3f213b72bd23ea6d36a22547d410ebf686a28
Juan <juanpvcoc@gmail.com>
2024-08-10 13:33:40 -0500

    Commit para montar la app

---
+++ App.js
@@ -0,0 +1,49 @@
+import { useEffect } from 'react';
+import { Col, Spin } from 'antd';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import Searcher from './components/Searcher';
+import PokemonList from './components/PokemonList';
+import { getPokemon } from './api';
+import { getPokemonsWithDetails, setLoading } from './actions';
+import logo from './statics/logo.svg';
+import './App.css';
+
+function App() {
+  const pokemons = useSelector((state) =>
+    state.getIn(['data', 'pokemons'], shallowEqual)
+  ).toJS();
+
+  const loading = useSelector((state) => state.getIn(['ui', 'loading']));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      dispatch(setLoading(true));
+      const pokemonsRes = await getPokemon();
+      dispatch(getPokemonsWithDetails(pokemonsRes));
+      dispatch(setLoading(false));
+    };
+
+    fetchPokemons();
+  }, []);
+
+  return (
+    <div className='App'>
+      <Col span={4} offset={10}>
+        <img src={logo} alt='Pokedux' />
+      </Col>
+      <Col span={8} offset={8}>
+        <Searcher />
+      </Col>
+      {loading ? (
+        <Col offset={12}>
+          <Spin spinning size='large' />
+        </Col>
+      ) : (
+        <PokemonList pokemons={pokemons} />
+      )}
+    </div>
+  );
+}
+
+export default App;
